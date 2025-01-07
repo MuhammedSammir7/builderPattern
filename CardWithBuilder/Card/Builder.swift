3a84cb5589b5e50cbe680a5847fdd85cd5b5be1e
MuhammedSammir7 <mmss2012377@gmail.com>
2025-01-08 00:33:36 +0200

    Initial commit

---
+++ CardWithBuilder/Card/Builder.swift
@@ -0,0 +1,8 @@
+//
+//  Builder.swift
+//  CardWithBuilder
+//
+//  Created by ios on 07/01/2025.
+//
+
+import Foundation
---
+++ CardWithBuilder/Card/CardCustomeView.swift
@@ -0,0 +1,20 @@
+//
+//  CardCustomeView.swift
+//  CardWithBuilder
+//
+//  Created by ios on 07/01/2025.
+//
+
+import UIKit
+
+class CardCustomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
